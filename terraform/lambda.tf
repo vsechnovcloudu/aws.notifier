289d9c3b9c734a15395bb0592a04a2257d01b380
David Kubec <davesade.42@gmail.com>
2018-05-16 22:45:17 +0200

    Path for API invocation corrected.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -27,5 +27,5 @@ resource "aws_lambda_permission" "allow-apigw" {
   action         = "lambda:InvokeFunction"
   function_name  = "${aws_lambda_function.emailsender.function_name}"
   principal      = "apigateway.amazonaws.com"
-  source_arn     = "arn:aws:execute-api:${var.REGION}:${var.aws_account_id}:${aws_api_gateway_rest_api.sender.id}/*/${aws_api_gateway_method.senderpost.http_method}/"
+  source_arn     = "arn:aws:execute-api:${var.REGION}:${var.aws_account_id}:${aws_api_gateway_rest_api.sender.id}/*/${aws_api_gateway_method.senderpost.http_method}${aws_api_gateway_resource.sender.path}"
 }
