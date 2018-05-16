14bdf0531e861c655dc762b816e46c17dc414534
David Kubec <davesade.42@gmail.com>
2018-05-16 22:25:56 +0200

    Added CORS.

--- terraform/api.tf
+++ terraform/api.tf
@@ -59,6 +59,56 @@ resource "aws_api_gateway_integration_response" "senderpost" {
   }
 }
 
+resource "aws_api_gateway_method" "cors-sender" {
+  rest_api_id = "${aws_api_gateway_rest_api.sender.id}"
+  resource_id = "${aws_api_gateway_resource.sender.id}"
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors-sender" {
+  rest_api_id = "${aws_api_gateway_rest_api.sender.id}"
+  resource_id = "${aws_api_gateway_resource.sender.id}"
+  http_method = "${aws_api_gateway_method.cors-sender.http_method}"
+  type = "MOCK"
+  request_templates {
+      "application/json" = <<EOF
+        {
+        "statusCode" : 200
+        }
+        EOF
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors-sender" {
+  depends_on  = ["aws_api_gateway_integration.cors-sender"]
+  rest_api_id = "${aws_api_gateway_rest_api.sender.id}"
+  resource_id = "${aws_api_gateway_resource.sender.id}"
+  http_method = "${aws_api_gateway_method.cors-sender.http_method}"
+  status_code = "${aws_api_gateway_method_response.cors-sender.status_code}"
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.origin}'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "cors-sender" {
+  depends_on  = ["aws_api_gateway_method.cors-sender"]
+  rest_api_id = "${aws_api_gateway_rest_api.sender.id}"
+  resource_id = "${aws_api_gateway_resource.sender.id}"
+  http_method = "${aws_api_gateway_method.cors-sender.http_method}"
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
 resource "aws_api_gateway_deployment" "sender" {
   depends_on = ["aws_api_gateway_integration.senderpost"]
   rest_api_id = "${aws_api_gateway_rest_api.sender.id}"
